e95e1f642fa2ca95d486d1734230091e907e8920
shengshenghuo <shengshenghuo3@gmail.com>
2024-11-20 14:22:19 -0800

    done with navigation css part

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -2,17 +2,33 @@ import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
-function Navigation() {
-  return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
+// import { useSelector } from "react-redux";
 
-      <li>
-        <ProfileButton />
-      </li>
-    </ul>
+// function Navigation() {
+//   return (
+//     <ul>
+//       <li>
+//         <NavLink to="/">Home</NavLink>
+//       </li>
+
+//       <li>
+//         <ProfileButton />
+//       </li>
+//     </ul>
+//   );
+// }
+
+function Navigation() {
+  // const user = useSelector((store)=> store.session.user);
+
+  return (
+    <div className="wrapper">
+      <div className="nav-link-area">
+        <NavLink to="/">Home</NavLink>
+        <div><ProfileButton /></div>
+      </div>
+
+    </div>
   );
 }
 
