5803cde2572d8525cbfeb3bfd5177cae7a0af878
shengshenghuo <shengshenghuo3@gmail.com>
2024-12-01 23:28:12 -0800

    latest commit change vite.config.js and init file

--- react-vite/vite.config.js
+++ react-vite/vite.config.js
@@ -1,3 +1,25 @@
+// import { defineConfig } from "vite";
+// import eslintPlugin from "vite-plugin-eslint";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig((mode) => ({
+//   plugins: [
+//     react(),
+//     eslintPlugin({
+//       lintOnStart: true,
+//       failOnError: mode === "production",
+//     }),
+//   ],
+//   server: {
+//     open: true,
+//     proxy: {
+//       "/api": "http://127.0.0.1:8000",
+//     },
+//   },
+// }));
+
+
 import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
@@ -11,6 +33,19 @@ export default defineConfig((mode) => ({
       failOnError: mode === "production",
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split node_modules into separate chunks
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        chunkSizeWarningLimit: 600
+      }
+    }
+  },
   server: {
     open: true,
     proxy: {
